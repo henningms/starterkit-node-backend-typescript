6dc62bafe15f7d4c2a4ea320e0148afc0957e83f
Henning Mosand Stephansen <henningmosand@outlook.com>
2016-10-13 13:39:49 +0200

    Makes it more typescripty

---
+++ src/app.ts
@@ -0,0 +1,4 @@
+import Server from './server';
+
+const appServer = Server.create();
+appServer.start();
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,50 @@
 import * as Express from 'express';
 
-var app = Express();
+export default class Server {
+    public app: Express.Application;
 
-app.get('/', (request, response) => {
-    response.send('Hello world!');
-});
+    private static defaultPort: number = 3000;
 
-let port = 4500;
+    /**
+     * Creates a new instance of the server with an optional port parameter
+     */
+    public static create(port: number = Server.defaultPort): Server {
+        return new Server(port);
+    }
 
-app.listen(port, () => {
-    console.log(`App listening on ${port}`);
-});
+    /**
+     * Construct instance of server with the desired port
+     * readonly indicates that we want the parameter to be turned
+     * into a readonly property
+     */
+    constructor(readonly port: number) {
+        this.app = Express();
+
+        this.configureServer();
+        this.setUpRoutes();
+    }
+
+    /**
+     * Configures server options
+     */
+    private configureServer(): void {
+    }
+
+    /**
+     * Sets up routes
+     */
+    private setUpRoutes(): void {
+        this.app.get('/', (request, response) => {
+            response.send("Hello world!");
+        });
+    }
+
+    /**
+     * Starts the server
+     */
+    public start(): void {
+        this.app.listen(this.port, () => {
+            console.log(`App listening on ${this.port}`);
+        });
+    }
+}
