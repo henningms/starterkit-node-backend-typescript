929d17cb9521b2d33f2490623eefeb10aed092e0
Henning Mosand Stephansen <henningmosand@outlook.com>
2016-10-13 11:16:21 +0200

    Get debugging working correctly in VS Code

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,22 @@
 var gulp = require('gulp');
-var tsc = require('gulp-typescript');
+var tsc  = require("gulp-typescript");
 var del = require('del');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 var path = require('path');
-
+ 
 var tsProject = tsc.createProject("tsconfig.json");
-
-gulp.task('clean', function (cb) {
-    return del('dist', cb);
-});
-
+ 
+gulp.task('clean', function(cb){
+    return del('dist', cb)    
+})
+ 
 gulp.task('build', ['clean'], function() {
-    var tsResult = gulp.src(["typings/index.d.ts", "src/**/*.ts"])
-        .pipe(sourcemaps.init())
-        .pipe(tsProject());
-
-    return tsResult.js
-        .pipe(sourcemaps.write('.', {
-            sourceRoot: function (file) {return file.cwd + "/src";}
-        }))
-        .pipe(gulp.dest("dist"));
+    var tsResult = gulp.src(["typings/**/*.ts","src/**/*.ts"])
+        .pipe(sourcemaps.init())
+        .pipe(tsProject()).js
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest("dist"));
 });
-
+ 
 gulp.task('default', ['build']);
